bf28b812a1dc74b6359710097a5bca4114b3e2b8
Omer Faruk Dogan <omer.crlys.44@gmail.com>
2022-04-10 23:05:08 +0300

    @Interceptors

---
+++ src/main/java/com/qubitfaruk/aop/stereotype/StereoTypeBean.java
@@ -0,0 +1,15 @@
+package com.qubitfaruk.aop.stereotype;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MyStereoType
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StereoTypeBean {
+    private Long id;
+    private String name;
+    private String trade;
+}
